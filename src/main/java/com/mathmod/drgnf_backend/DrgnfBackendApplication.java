69e402cbd747263f7312cf13b5fa5b6f6ebc6feb
Grubenko Maxim <mdgrubenko@maieducation>
2023-05-11 11:11:58 +0300

    Initial commit

---
+++ src/main/java/com/mathmod/drgnf_backend/DrgnfBackendApplication.java
@@ -0,0 +1,13 @@
+package com.mathmod.drgnf_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrgnfBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DrgnfBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mathmod/drgnf_backend/DrgnfBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mathmod.drgnf_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DrgnfBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
